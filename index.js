e6bb5b1bcb7ae566f8ae11d78add091614bcd10d
Cory <corymclean0@gmail.com>
2023-08-14 19:43:43 -0400

    Wow

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);*/
+
+const thankList=[]
+const toThank= ["Guadalupe","Ollie","Aki"]
+function writeCards(toThank){ for(let n=0; n<toThank.length; n++)
+    { thankList.push(`Thank you, ${toThank[n]}, for the wonderful surprise gift!`)}
+    
+        return thankList
+
+}
+
+function countDown(j){while(j>-1){console.log(j--)}}
+
+
+
+
+
